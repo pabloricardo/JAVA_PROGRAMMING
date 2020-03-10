fa00e5b7c6104a552dcca2cb46a63e7b3afda65a
pablo.pereira <pablo.pereira@sigga.com>
2020-03-10 17:01:19 -0300

    add new tests in string

--- TestString/src/app/App.java
+++ TestString/src/app/App.java
@@ -2,9 +2,21 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String file = "SiggaImage.png";
-        System.out.println(file.indexOf("."));
-        file = file.substring(0 , file.indexOf("."));
-        System.out.println(file);
+        // String file = "Test.png";
+        // System.out.println(file.indexOf("."));
+        // file = file.substring(0 , file.indexOf("."));
+        // System.out.println(file);
+
+        String aux = "TeSt StRinG FoR TestS %s           ";
+        String str01 = aux.toUpperCase();
+        String str02 = aux.toLowerCase();
+        String str03 = aux.trim();
+        String str04 = aux.replace('T', 'B');
+        String str05 = aux.replaceAll("TeSt", "Alter");
+        System.out.println("ToUpperCase -"+str01+"-");
+        System.out.println("ToLowerCase -"+str02+"-");
+        System.out.println("Trim -"+str03+"-");
+        System.out.println("Replace -"+str04+"-");
+        System.out.println("Replace -"+str05+"-");
     }
 }
