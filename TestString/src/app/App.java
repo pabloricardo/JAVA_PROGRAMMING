4c660676580339169d22cd817c7cdd7cf6d23d9a
PabloRP <pablo.mg@hotmail.com>
2020-03-10 21:03:43 -0300

    Add new examples TestString

--- TestString/src/app/App.java
+++ TestString/src/app/App.java
@@ -13,10 +13,14 @@ public class App {
         String str03 = aux.trim();
         String str04 = aux.replace('T', 'B');
         String str05 = aux.replaceAll("TeSt", "Alter");
+        int str06 = aux.indexOf("F");
+        String str07 = aux.substring(2, 12);
         System.out.println("ToUpperCase -"+str01+"-");
         System.out.println("ToLowerCase -"+str02+"-");
         System.out.println("Trim -"+str03+"-");
         System.out.println("Replace -"+str04+"-");
         System.out.println("Replace -"+str05+"-");
+        System.out.println("IndexOf -"+str06+"-");
+        System.out.println("Substring -"+str07+"-");
     }
 }
