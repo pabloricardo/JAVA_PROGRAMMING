676206381bb3fc0286ab31638f23643c0053a437
PabloRP <pablo.mg@hotmail.com>
2020-04-20 22:35:36 -0300

    Complemented comparator example

--- Comparator/src/main/java/application/App.java
+++ Comparator/src/main/java/application/App.java
@@ -3,6 +3,7 @@ package application;
 import entities.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -15,8 +16,23 @@ public class App {
         listProducts.add(new Product("Notebook", 1200.00));
         listProducts.add(new Product("Tablet", 400.00));
 
-        //Example using created class comparator
-        listProducts.sort(new MyComparator());
+        //Example using the created class comparator
+        //listProducts.sort(new MyComparator());
+
+        //Example using the anonymous class
+        /*Comparator<Product> comp = new Comparator<Product>(){
+            public int compare(Product p1, Product p2){
+                return p1.getNome().toUpperCase().compareTo(p2.getNome());
+            }
+        };
+        listProducts.sort(comp);*/
+
+        //Comparator example using the expression lambda without keys
+        /*Comparator<Product> comp = (p1,p2) -> p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());
+        listProducts.sort(comp);*/
+
+        //Comprator usando a expressão lambda
+        listProducts.sort((p1,p2) -> p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase()));
 
         for (Product product : listProducts) {
             System.out.println(product);
