7851fc8a232d43d74aea48c6ac92f27d3fb0ae95
PabloRP <pablo.mg@hotmail.com>
2020-04-22 20:15:06 -0300

    add new stream example

--- ExampleStream/src/main/java/application/App.java
+++ ExampleStream/src/main/java/application/App.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -11,9 +12,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        Stream<Integer> str = Stream.of(2,3,5,6,7);
+        List<Integer> list = Arrays.asList(3,4,5,10,7);
+        Stream<Integer> listStream = list.stream();
+        System.out.println(Arrays.toString(listStream.toArray()));
 
-        System.out.println(Arrays.toString(str.toArray()));
+        Stream<Integer> str1 = Stream.of(2,3,5,6,7).map(x -> x * 10);
+        System.out.println(Arrays.toString(str1.toArray()));
+
+        Stream<String> st2 = Stream.of("Bruna", "Pedro", "Carlos");
+        System.out.println(Arrays.toString(st2.toArray()));
+
+        Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
+        System.out.println(Arrays.toString(st3.limit(10).toArray()));
 
     }
 }
