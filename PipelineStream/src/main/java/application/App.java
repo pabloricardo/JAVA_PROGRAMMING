fd3bf5b312eff2706b5f890a000e7fee52152521
PabloRP <pablo.mg@hotmail.com>
2020-04-25 18:00:58 -0300

    add example pipeline

--- PipelineStream/src/main/java/application/App.java
+++ PipelineStream/src/main/java/application/App.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<Integer> list = Arrays.asList(2,4,7,9);
+        Stream<Integer> st1 = list.stream().map(x -> x * 10);
+        System.out.println(Arrays.toString(st1.toArray()));
+
+        int sum = list.stream().reduce(0, (x,y) -> x + y);
+        System.out.println("Sum: " + sum);
+
+        List<Integer> newList = list.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 10)
+                .collect(Collectors.toList());
+
+        System.out.println(Arrays.toString(newList.toArray()));
+
     }
 }
