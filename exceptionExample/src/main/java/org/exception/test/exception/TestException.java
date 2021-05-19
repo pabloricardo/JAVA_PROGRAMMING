4aaa6d421f00ca0d15e098df884feca4face3b67
PabloRP <pablo.mg@hotmail.com>
2021-05-18 21:01:21 -0300

    added more examples

---
+++ exceptionExample/src/main/java/org/exception/test/exception/TestException.java
@@ -0,0 +1,4 @@
+package org.exception.TestException;
+
+public class TestException {
+}
---
+++ genericosDelimitados/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        List<Integer> list = new ArrayList<>();
+
+        String userDir = System.getProperty("user.dir");
+        System.out.println(userDir);
+
+    }
+}
